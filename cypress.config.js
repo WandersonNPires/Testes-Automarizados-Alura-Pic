f19955a076a319000514d722be7c8c1b3fb78fd8
Wanderson Neiva <wandersonneivapires@gmail.com>
2024-01-26 08:48:47 -0300

    Adição  biblioteca mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,17 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require ("cypress");
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor',cucumber())
+      {"reporter"; "mochawesome",
+      "reporterOptions";
+      {"reportDir";"cypress/report/mochawesome-report",
+      "overwrite";true,
+      "html";true;
+      "json"; false;
+      "timestamp"; "mmddyyyy_HHMMss"}}
     },
     specPattern: "cypress/e2e/step_definitions/*.feature"
   },
